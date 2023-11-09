d71399feab313bc6bf73239915fa2c020036b98a
Zoe Zhang <jane@example.com>
2023-11-08 23:49:53 -0800

    update the function

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,5 +1,16 @@
-# strsplit1 function aims to seperate the strings of a character
-strsplit1 <- function (x, split) {
-  strsplit(x, split=split) [[1]]
+
+#' Split a string
+#'
+#' @param x A character vector with one element.
+#' @param split What to split on.
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' strsplit1(x, split = ",")
+strsplit1 <- function(x, split) {
+  strsplit(x, split = split)[[1]]
 }
 
